3fd09ff9b1eba457f08c1974e5fc434bc6a5bafa
gustavocoiimbra <106631464+gustavocoiimbra@users.noreply.github.com>
2022-11-14 16:00:46 -0300

    ativação do botão 'Notify Me' e renomeação para 'Ativar Notificação'

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,16 +5,19 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('O produto foi compartilhado!');
+  }
+
+  onNotify() {
+    window.alert('Você será notificado quando o produto entrar em promoção!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
